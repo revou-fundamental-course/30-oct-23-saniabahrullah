45ea6aba63e6e82e662ce37f7a3ee5faf2d22d6a
Sania Bahrullah <saniabahrullah@gmail.com>
2023-11-09 21:11:22 +0700

    Pesan komit

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
 function calculateBMI() {
-    // Mengambil nilai input dari form
     var age = document.getElementById("age").value;
     var gender = document.getElementById("gender").value;
     var height = document.getElementById("height").value;
     var weight = document.getElementById("weight").value;
 
-    // Melakukan perhitungan BMI
-    var heightMeters = height / 100; // Mengkonversi tinggi dari cm ke meter
+    var heightMeters = height / 100;
     var bmi = weight / (heightMeters * heightMeters);
 
-    // Menampilkan hasil BMI
     var bmiResult = document.getElementById("bmi");
     bmiResult.innerHTML = "BMI: " + bmi.toFixed(2);
 
-    // Menyediakan interpretasi BMI berdasarkan kategori
     var interpretation = document.getElementById("interpretation");
     if (bmi < 18.5) {
-        interpretation.innerHTML = "Underweight";
+        interpretation.innerHTML = "Kekurangan Berat Badan";
     } else if (bmi >= 18.5 && bmi < 24.9) {
-        interpretation.innerHTML = "Normal weight";
+        interpretation.innerHTML = "Normal";
     } else if (bmi >= 25 && bmi < 29.9) {
-        interpretation.innerHTML = "Overweight";
+        interpretation.innerHTML = "Kelebihan Berat Badan";
     } else {
-        interpretation.innerHTML = "Obesity";
+        interpretation.innerHTML = "Kegemukan";
     }
 
-    // Menambahkan informasi usia dan jenis kelamin ke hasil BMI
-    bmiResult.innerHTML += "<br>Age: " + age + " years";
-    bmiResult.innerHTML += "<br>Gender: " + gender;
+    bmiResult.innerHTML += "<br>Usia: " + age + " tahun";
+    bmiResult.innerHTML += "<br>Jenis Kelamin: " + (gender === "male" ? "Laki-Laki" : "Wanita");
 }
